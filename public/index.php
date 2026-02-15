ee717c5e054c16a2e20bbcbbf719ba5b2f3a3d64
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2026-02-15 17:54:47 -0600

    debug

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@ require_once '../core/Router.php';
 require_once '../resources/v1/UserResource.php';
 require_once '../resources/v1/ProductResource.php';
 
+echo "API is running...";
+
 $scriptName = dirname($_SERVER['SCRIPT_NAME']);
 $basePath = $scriptName;
 
